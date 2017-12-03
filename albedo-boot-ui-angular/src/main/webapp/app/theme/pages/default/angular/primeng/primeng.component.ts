8a32e14ecdc16ce285d4614f41eee3345b5b0935
somewhere <837158334@qq.com>
2017-12-03 13:20:25 +0800

    angular 模塊提交

--- albedo-boot-ui-angular/src/main/webapp/app/theme/pages/default/angular/primeng/primeng.component.ts
+++ albedo-boot-ui-angular/src/main/webapp/app/theme/pages/default/angular/primeng/primeng.component.ts
@@ -5,8 +5,8 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
     templateUrl: "./primeng.component.html",
     encapsulation: ViewEncapsulation.None,
     styleUrls: [
-        '../../../../../../../node_modules/primeng/resources/primeng.css',
-        '../../../../../../../node_modules/primeng/resources/themes/bootstrap/theme.css',
+        '../../../../../../../../../node_modules/primeng/resources/primeng.css',
+        '../../../../../../../../../node_modules/primeng/resources/themes/bootstrap/theme.css',
     ]
 })
 export class PrimengComponent implements OnInit {
